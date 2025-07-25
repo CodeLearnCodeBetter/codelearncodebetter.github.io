c53c0ff2d3b41bc1645db3557c745e997cc4fedc
Kevin Scheepers <kevinscheepers49@gmail.com>
2025-07-25 20:01:37 +0200

    Comments removed

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
     PDFObject.embed("Resume.pdf#toolbar=0&navpanes=0&scrollbar=0", "#pdfRenderer");
-    const container = document.getElementById("pdfRenderer");
-//     console.log("container:" + container);
-//     const height = container.style.width * 0.707;
-//     container.style.width = height + "px";
-//     PDFObject.embed("Kevin Scheepers CV-5.pdf", "#pdfRenderer");
-//     console.log("width" + container.style.width);
-// //   const header = document.getElementById("header");
-//   const pdf = document.getElementById("pdfRenderer");
-//     debugger;
-//   const heightA = header.offsetHeight; // gets computed height of A
-//   pdf.style.height = ("100vh" + heightA);
 });
